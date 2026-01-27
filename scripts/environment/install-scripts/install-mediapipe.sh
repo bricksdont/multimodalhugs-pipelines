ad47fe8ad28c303b9d6aacf23aa26abcbb8accf4
catherine-o-brien <catherineelizabeth.obrien@uzh.ch>
2026-01-27 17:51:18 +0100

    Changed install.sh to individual files for each estimator

---
+++ scripts/environment/install-scripts/install-mediapipe.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+module load gpumem32gb cuda/13.0.2 cudnn/9.8.0.87-12 miniforge3
+
+environment_scripts=$(dirname "$0")
+scripts=$environment_scripts/../..
+base=$scripts/..
+
+venvs=$base/venvs
+
+############################
+# mediapipe holistic (py39)
+############################
+source activate $venvs/mediapipe
+tools=$base/tools/mediapipe
+mkdir -p $tools
+
+pip install pose-format
+pip install "mediapipe<0.10.30"
+
+# install multimodalhugs
+
+git clone https://github.com/GerrySant/multimodalhugs.git $tools/multimodalhugs
+
+# pin commit  https://github.com/GerrySant/multimodalhugs/commit/5201c80f27aa70c460e8297a799dc5daccbd1b3b
+# to avoid unintentionally breaking the code
+
+(cd $tools/mediapipe && git checkout "5201c80f27aa70c460e8297a799dc5daccbd1b3b")
+
+(cd $tools/mediapipe && pip install .)
+
+# TF keras, because keras 3 is not supported in Transformers
+
+pip install tf-keras
+
+# bleurt not supported out of the box with evaluate
+
+pip install git+https://github.com/google-research/bleurt.git
+
+# openGL is no longer available on the cluster
+
+OPENCV_VERSION=$(python - <<'EOF'
+import importlib.metadata as m
+try:
+    print(m.version("opencv-python"))
+except m.PackageNotFoundError:
+    print(m.version("opencv-python-headless"))
+EOF
+)
+
+pip install tensorflow==2.13.1 mediapipe==0.10.9 protobuf==3.20.3
+
+pip uninstall -y opencv-python opencv-python-headless
+pip install "opencv-python-headless==${OPENCV_VERSION}"
+
+conda deactivate 
---
+++ scripts/environment/install-scripts/install-mmposewholebody.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+############################
+# mmposewholebody (py38)
+############################
+
+source activate $venvs/mmposewholebody
+tools=$base/tools/mmposewholebody
+mkdir -p $tools
+
+# install fork of pose-format that extends to mmposewholebody
+
+pip uninstall -y pose-format
+git clone -b new_estimators https://github.com/catherine-o-brien/pose.git $tools/pose
+cd $tools/pose/src/python
+pip install -e .
+
+# install dependencies for mmposewholebody
+module load cuda/12.6.3 # required by mmcv-full
+conda install pytorch torchvision 
+pip install openmim
+mim install mmengine
+mim install mmcv-full
+mim install "mmdet>=3.1.0"
+mim install mmpose
+
+# install multimodalhugs
+
+git clone https://github.com/GerrySant/multimodalhugs.git $tools/multimodalhugs
+
+# pin commit  https://github.com/GerrySant/multimodalhugs/commit/5201c80f27aa70c460e8297a799dc5daccbd1b3b
+# to avoid unintentionally breaking the code
+
+(cd $tools/multimodalhugs && git checkout "5201c80f27aa70c460e8297a799dc5daccbd1b3b")
+
+(cd $tools/multimodalhugs && pip install .)
+
+# install SL datasets
+
+pip install git+https://github.com/sign-language-processing/datasets.git
+
+# TF keras, because keras 3 is not supported in Transformers
+
+pip install tf-keras
+
+# bleurt not supported out of the box with evaluate
+
+pip install git+https://github.com/google-research/bleurt.git
+
+# openGL is no longer available on the cluster
+
+OPENCV_VERSION=$(python - <<'EOF'
+import importlib.metadata as m
+try:
+    print(m.version("opencv-python"))
+except m.PackageNotFoundError:
+    print(m.version("opencv-python-headless"))
+EOF
+)
+
+pip uninstall -y opencv-python opencv-python-headless
+pip install "opencv-python-headless==${OPENCV_VERSION}"
